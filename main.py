ff14dda5630879420ff97b3884c85ed033f37528
BlazeWasHere <blaze@dorime.org>
2021-07-22 20:48:16 +0000

    linting files & adding shebang.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # BoilerBot V1.1
 # Written by Cole^2
 
-import discord
 from discord.ext import commands
+import discord
+
 import re
+
 from classes import Parser, Course
 
 description = "BoilerBot"
 
 bot = commands.Bot(command_prefix='?', description=description)
 
+
 def parseCourseInfo(subject, number):
-    parser = Parser(subject,number)
+    parser = Parser(subject, number)
     data = parser.getCourseInfo()
     title = data.find("h1", {"id": "course_preview_title"}).text
     description = data.find("hr").next_sibling
@@ -20,16 +26,20 @@ def parseCourseInfo(subject, number):
     course = Course(title, description, credits)
     return course
 
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
+
 @bot.command()
 async def courseinfo(ctx, subject, number):
     course = parseCourseInfo(subject, number)
-    embed = discord.Embed(title=course.getTitle(), description=course.getDescription(), color=0xdbce14)
-    embed.add_field(name="Credit Hours", value=course.getCredits(), inline=False)
+    embed = discord.Embed(title=course.getTitle(),
+                          description=course.getDescription(), color=0xdbce14)
+    embed.add_field(name="Credit Hours",
+                    value=course.getCredits(), inline=False)
     await ctx.send(embed=embed)
 
 bot.run('. . .')
