1c20f9804d2f142daf36d1f7e06e0ff1e8d6b09c
Cole Brooks <macoryx@gmail.com>
2021-07-22 15:27:04 -0400

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Course:
+    def __init__(self, title, description, credits):
+        self.title = title
+        self.description = description
+        self.credits = credits
+    def getTitle(self):
+        return self.title
+    def getDescription(self):
+        return self.description
+    def getCredits(self):
+        return self.credits
+
+class Parser:
+    def __init__(self, subject, number):
+        self.subject = subject
+        self.number = number
+    def getCourseUrl(self):
+        req = requests.get("https://catalog.purdue.edu/search_advanced.php?cur_cat_oid=14&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=33&filter%5Bkeyword%5D={0} {1}&filter%5Bexact_match%5D=1".format(self.subject, self.number), verify=False)
+        soup = BeautifulSoup(req.text, "lxml")
+        courseUrl = "https://catalog.purdue.edu/" + soup.find_all("td", class_="td_dark")[1].find_all("a")[0]['href'].replace("_nopop", "")
+        return courseUrl
+    def getCourseInfo(self):
+        courseUrl = self.getCourseUrl()
+        req = requests.get(courseUrl, verify=False)
+        soup = BeautifulSoup(req.text, "lxml")
+        courseData = soup.find_all("td", class_="block_content_popup")[0]
+        return courseData
