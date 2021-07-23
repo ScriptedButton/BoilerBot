d519b05693a555dc259f1cfd57210b61cae2151a
BlazeWasHere <blaze@dorime.org>
2021-07-23 15:56:56 +0000

    disable insecure request warning from urllib3

--- classes.py
+++ classes.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from dataclasses import dataclass
+import urllib3
 
 from bs4 import BeautifulSoup, element
 import requests
+
+# disable 'InsecureRequestWarning: Unverified HTTPS request' warning
+# TODO find a way to verify with purdue server.
+urllib3.disable_warnings()
 
 
 @dataclass
@@ -21,7 +26,9 @@ class Parser:
 
     def getCourseUrl(self) -> str:
         req = requests.get(
-            f"https://catalog.purdue.edu/search_advanced.php?cur_cat_oid=14&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=33&filter%5Bkeyword%5D={self.subject} {self.number}&filter%5Bexact_match%5D=1")
+            f"https://catalog.purdue.edu/search_advanced.php?cur_cat_oid=14&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=33&filter%5Bkeyword%5D={self.subject} {self.number}&filter%5Bexact_match%5D=1",
+            verify=False
+        )
 
         soup = BeautifulSoup(req.text, "lxml")
         courseUrl = "https://catalog.purdue.edu/" + \
@@ -33,6 +40,8 @@ class Parser:
     def getCourseInfo(self) -> element.Tag:
         courseUrl = self.getCourseUrl()
         req = requests.get(courseUrl, verify=False)
+
         soup = BeautifulSoup(req.text, "lxml")
         courseData = soup.find_all("td", class_="block_content_popup")[0]
+
         return courseData
