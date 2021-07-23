dabfec89ded3abd846057868f29435c556f72d25
BlazeWasHere <blaze@dorime.org>
2021-07-23 15:45:04 +0000

    add type hints

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@
 
 from dataclasses import dataclass
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import requests
 
 
@@ -15,20 +15,22 @@ class Course:
 
 
 class Parser:
-    def __init__(self, subject, number):
+    def __init__(self, subject: str, number: int) -> None:
         self.subject = subject
         self.number = number
 
-    def getCourseUrl(self):
-        req = requests.get("https://catalog.purdue.edu/search_advanced.php?cur_cat_oid=14&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=33&filter%5Bkeyword%5D={0} {1}&filter%5Bexact_match%5D=1".format(
-            self.subject, self.number), verify=False)
+    def getCourseUrl(self) -> str:
+        req = requests.get(
+            f"https://catalog.purdue.edu/search_advanced.php?cur_cat_oid=14&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=33&filter%5Bkeyword%5D={self.subject} {self.number}&filter%5Bexact_match%5D=1")
+
         soup = BeautifulSoup(req.text, "lxml")
         courseUrl = "https://catalog.purdue.edu/" + \
             soup.find_all("td", class_="td_dark")[1].find_all(
                 "a")[0]['href'].replace("_nopop", "")
+
         return courseUrl
 
-    def getCourseInfo(self):
+    def getCourseInfo(self) -> element.Tag:
         courseUrl = self.getCourseUrl()
         req = requests.get(courseUrl, verify=False)
         soup = BeautifulSoup(req.text, "lxml")
